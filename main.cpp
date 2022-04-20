6d9237b1262e532988e0010d656d6aa3f347bfcb
patrikk0123 <patrik.korytar@gmail.com>
2022-04-20 20:47:06 +0200

    Added SIGINT handling

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include "src/utils.h"
 #include "src/sniffer.h"
 
+#include <signal.h>
+
+/**
+ * Handle SIGINT signal by exiting
+ * with zero code.
+ * @param pid process ID 
+ */
+void sigint_handle(int pid);
+
 int main(int argc, char*argv[])
 {
+  signal(SIGINT, sigint_handle);
+
   Arguments args = get_args(argc, argv);
   if(args.interface == "") {
     print_interfaces();
@@ -12,4 +23,11 @@ int main(int argc, char*argv[])
   }
 
   return 0;
+}
+
+void sigint_handle(int pid)
+{
+  (void)pid;
+
+  exit(0);
 }
